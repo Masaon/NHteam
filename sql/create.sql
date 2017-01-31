952be9dc267bd3da453df21e6e0b73d91be0d886
Masaon <bennpo222@gmail.com>
2017-01-31 13:00:29 +0900

    Update create.sql

--- sql/create.sql
+++ sql/create.sql
@@ -9,6 +9,6 @@ grant create view to nh;
 grant create session to nh;
 
 
-drop table wiki_page;
-create table wiki_page(name varchar2(100) primary key,content varchar2(2000),update_time varchar2(100) default sysdate);
+drop table nh.wiki_page;
+create table nh.wiki_page(name varchar2(100) primary key,content varchar2(2000),update_time varchar2(100) default sysdate);
 
