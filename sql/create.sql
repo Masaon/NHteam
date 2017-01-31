007d7001a43e7edc5671b83bf39733a5ae4667fd
Masaon <bennpo222@gmail.com>
2017-01-31 13:50:26 +0900

    Update create.sql

--- sql/create.sql
+++ sql/create.sql
@@ -1,15 +1,15 @@
 conn sys as sysdba;
 
-drop user nh;
+drop user nh cascade;
 create user nh identified by rm;
 
 grant create table to nh;
 grant create user to nh;
 grant create view to nh;
 grant create session to nh;
+grant dba to nh;
 
-conn nh / rm;
+conn nh /rm;
 
 drop table wiki_page;
 create table wiki_page(name varchar2(100) primary key,content varchar2(2000),update_time varchar2(100) default sysdate);
-
